eb2abc9f91f2a70f6f4ea20be916ab7c4c82bb64
JoelEnrique <19670128@itiguala.edu.mx>
2023-09-11 10:44:53 -0600

    Comencemos

---
+++ Introduccion.py
@@ -0,0 +1,25 @@
+'''
+Bienvenidos al curso de programación,
+donde exploraremos el fascinante mundo de la creación de software y su importancia en la actualidad.
+La programación es una disciplina esencial en la era digital en la que vivimos.
+En este curso, nos sumergiremos en el universo de la codificación, donde aprenderemos a dar vida a
+nuestras ideas a través de la escritura de código. Desde el desarrollo de aplicaciones móviles hasta
+la automatización de tareas y la resolución de problemas complejos, la programación desempeña
+un papel fundamental en la tecnología que impulsa nuestro mundo.
+
+En un entorno cada vez más tecnológico y orientado hacia la digitalización, las habilidades de programación
+son altamente demandadas en una amplia gama de industrias. Ya sea que desees convertirte en un ingeniero de software,
+un científico de datos, un desarrollador web o simplemente desees comprender mejor el mundo digital que te rodea,
+este curso te proporcionará las bases necesarias para empezar tu viaje en el emocionante campo de la programación.
+
+A lo largo del curso, exploraremos los conceptos fundamentales de la programación y cómo aplicarlos para resolver
+problemas del mundo real. Descubriremos cómo crear aplicaciones, automatizar tareas repetitivas y aprovechar
+la potencia de la lógica de programación para mejorar nuestra eficiencia y creatividad en diversos campos.
+
+Así que prepárate para embarcarte en un viaje educativo emocionante donde adquirirás habilidades esenciales
+para prosperar en el mundo moderno. ¡La programación está en todas partes y es una herramienta poderosa que te
+abrirá un mundo de posibilidades!
+
+Joel Enrique Urquiza Martinez
+'''
+print("Hola raton sin cola")
